ff38333b49afa410c08733ab71f48485b4e9aded
b.ahmetrasim <b.ahmetrasim@DESKTOP-85E6FRT>
2018-01-27 17:10:42 +0300

    Add project files.

---
+++ BluetoothPoker/Deste52.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BluetoothPoker
+{
+    class Deste52
+    {
+       
+        public Deste52()
+        {
+            Dictionary<string, int> full = new Dictionary<string, int>();
+            string kupa; //Heart
+            string karo; //Diamonds
+            string sinek; //Clubs
+            string maca; //Spades
+            for (int i = 2; i < 14; i++)
+            {
+                if (i==10)
+                {
+                    kupa = "TH"; //Heart
+                    karo = "TD"; //Diamonds
+                    sinek = "TC"; //Clubs
+                    maca = "TS"; //Spades
+                }
+                if (i == 11)
+                {
+                    kupa = "JH"; //Heart
+                    karo = "JD"; //Diamonds
+                    sinek = "JC"; //Clubs
+                    maca = "JS"; //Spades
+
+                }
+                if (i == 12)
+                {
+                    kupa = "JH"; //Heart
+                    karo = "JD"; //Diamonds
+                    sinek = "JC"; //Clubs
+                    maca = "JS"; //Spades
+
+                }
+                if (i == 13)
+                {
+                    kupa = "JH"; //Heart
+                    karo = "JD"; //Diamonds
+                    sinek = "JC"; //Clubs
+                    maca = "JS"; //Spades
+                }
+                if (i < 10)
+                {
+                    kupa = i + "H"; //Heart
+                    karo = i + "D"; //Diamonds
+                    sinek = i + "C"; //Clubs
+                    maca = i + "S"; //Spades
+                }
+                
+            }
+        }
+    }
+}
